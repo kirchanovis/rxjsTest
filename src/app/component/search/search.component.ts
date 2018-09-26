ee56d8659938dca27c922c1af8d096192875c1f4
Ilya Kirchanov <kirchanovis@gmail.com>
2018-09-26 13:30:54 +0300

    update component Search

--- src/app/component/search/search.component.ts
+++ src/app/component/search/search.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Search } from '../../model/search';
 import { Observable } from 'rxjs/Observable';
+import { _throw } from 'rxjs/observable/throw';
 import { FormControl } from '@angular/forms';
-// import { of } from 'rxjs/observable/of';
-// import { concat } from 'rxjs/operators';;
-import { debounceTime, distinctUntilChanged, switchMap, every } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+import { timer } from 'rxjs/observable/timer';
+import { debounceTime, distinctUntilChanged, switchMap, map, retryWhen, delay } from 'rxjs/operators';
 import { SearchService } from './search.service';
 
 @Component({
@@ -15,19 +16,39 @@ import { SearchService } from './search.service';
 export class SearchComponent implements OnInit {
 
   searchResult$: Search[];
+  countResult$: Number;
   search: FormControl = new FormControl();
+  counter = 0;
+  error = false;
 
   constructor(private searchService: SearchService) {}
 
   ngOnInit() {
-    this.search.valueChanges
-    .pipe(
+    const first = this.search.valueChanges;
+
+    const second = first.pipe(
       debounceTime(1000),
       distinctUntilChanged(),
-      switchMap(query => this.searchService.getSearchResult(query))
-    ).subscribe(result => {
-      console.log(result, '1')
-      this.searchResult$ = result
+      map((value) => {
+        this.counter ++;
+        if (this.counter % 3 === 0) {
+          this.error = true;
+          throw value;
+        }
+        this.error = false;
+        return value;
+      }),
+      switchMap(query => this.searchService.getSearchResult(query)),
+      retryWhen(errors =>
+        errors.pipe(
+          delay(300)
+        )
+      )
+    );
+
+    second.subscribe(result => {
+      this.searchResult$ = result;
+      this.countResult$ = result.length;
     });
   }
 
